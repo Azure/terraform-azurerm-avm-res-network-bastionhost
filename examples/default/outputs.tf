9a8191441c7f4146a868be5acfd1d5cd1ed4fb60
Hermes <hemirafl@microsoft.com>
2024-02-19 11:12:42 +0800

    fixed outputs file

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -1,7 +1,9 @@
 output "bastion_host" {
-  value = module.azure_bastion.bastion-resource
+  value       = module.azure_bastion.bastion_resource
+  description = "The name of the bastion host resource"
 }
 
 output "bastion_host_id" {
-  value = module.azure_bastion.bastion-resource
+  value       = module.azure_bastion.bastion_resource
+  description = "The id of the bastion host resource"
 }
