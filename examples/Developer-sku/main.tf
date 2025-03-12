856936bc8fe0855c2a7c87cd77ae50c5d6db372b
Itamar Hirosh <itamarh@microsoft.com>
2025-03-12 09:39:24 +0200

    fix condition on pip

--- examples/Developer-sku/main.tf
+++ examples/Developer-sku/main.tf
@@ -49,12 +49,6 @@ module "virtualnetwork" {
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   address_space       = ["10.0.0.0/16"]
-  subnets = {
-    AzureBastionSubnet = {
-      name             = "AzureBastionSubnet"
-      address_prefixes = ["10.0.0.0/24"]
-    }
-  }
 }
 
 module "azure_bastion" {
