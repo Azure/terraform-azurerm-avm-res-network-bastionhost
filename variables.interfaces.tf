b02850bf8afb903acaf6a5dba4a2742ea4954234
Itamar Hirosh <itamarh@microsoft.com>
2025-04-10 12:40:57 +0300

    keeping interface for diagnostics settings for compliance

--- variables.interfaces.tf
+++ variables.interfaces.tf
@@ -17,6 +17,7 @@ variable "diagnostic_settings" {
     log_categories                           = optional(set(string), [])
     log_groups                               = optional(set(string), ["allLogs"])
     metric_categories                        = optional(set(string), ["AllMetrics"])
+    log_analytics_destination_type           = optional(string, "Dedicated")
     workspace_resource_id                    = optional(string, null)
     storage_account_resource_id              = optional(string, null)
     event_hub_authorization_rule_resource_id = optional(string, null)
@@ -26,6 +27,10 @@ variable "diagnostic_settings" {
   default  = {}
   nullable = false
 
+  validation {
+    condition     = alltrue([for _, v in var.diagnostic_settings : contains(["Dedicated", "AzureDiagnostics"], v.log_analytics_destination_type)])
+    error_message = "Log analytics destination type must be one of: 'Dedicated', 'AzureDiagnostics'."
+  }
   validation {
     condition = alltrue(
       [
